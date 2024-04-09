5ae4d54283ffb2694707789543782b51e43d38f8
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-04-09 22:38:09 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,22 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    """ Reddit API endpoint to get subreddit information """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'CustomBot/1.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        # Extract the JSON data from the response
+        data = response.json()
+        # Check if the subreddit exist
+        if 'data' in data and 'subscribers' in data['data']:
+            # Return the number of subscribers
+            return data['data']['subscribers']
+        else:
+            # If the subreddit does not exist or there was an error, return 0
+            return 0
+    else:
+        # If there was an error or the subreddit is invalid, return 0
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
